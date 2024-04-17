c18f657a91c194c12591aa5f18333ffb6d552954
juliog922 <juliogalindo922@gmail.com>
2024-04-17 21:45:01 +0200

    Added PostgreSQL tables for users, roles, and users_roles, including their schemas and models. Lessons 1-12 completed.

--- src/models.rs
+++ src/models.rs
@@ -42,3 +42,55 @@ pub struct NewCrate {
     pub version: String,
     pub description: Option<String>
 }
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct User {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    #[serde(skip_deserializing)]
+    pub created_at: NaiveDateTime
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name=users)]
+pub struct NewUser {
+    pub username: String,
+    pub password: String
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct Role {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub code: String,
+    pub name: String,
+    #[serde(skip_deserializing)]
+    pub created_at: NaiveDateTime
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name=roles)]
+pub struct NewRole {
+    pub code: String,
+    pub name: String
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+#[diesel(belongs_to(User))]
+#[diesel(belongs_to(Role))]
+pub struct UserRole {
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub user_id: i32,
+    pub role_id: i32
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name=users_roles)]
+pub struct NewUserRole {
+    pub user_id: i32,
+    pub role_id: i32
+}
+
